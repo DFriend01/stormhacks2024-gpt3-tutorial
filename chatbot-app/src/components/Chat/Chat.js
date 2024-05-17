915568cd60b7b29369155f54ffa7a5df6b576146
Devon Friend <devon.fri.2000@gmail.com>
2024-05-17 03:55:09 -0700

    Get chat completion to work

--- chatbot-app/src/components/Chat/Chat.js
+++ chatbot-app/src/components/Chat/Chat.js
@@ -1,20 +1,26 @@
 import React, { useState } from 'react';
+import OpenAI from "openai";
 import MessageList from './chat_children/MessageList/MessageList';
 import Input from './chat_children/Input/Input';
+import get_gpt_response from '../../services/chat_completion';
 import './Chat.css';
 
 const Chat = ({initialMessages}) => {
 
   const [messages, setMessages] = useState(initialMessages);
+  const [gptMessages, setGptMessages] = useState([]);
 
-  const addMessage = (sender, text, isLeft) => {
-    setMessages([...messages, {sender, text, isLeft }]);
+  const addMessages = (appendedMessages) => {
+    setMessages([...messages, ...appendedMessages]);
   };
 
   const onSendMessage = (text) => {
-    let sender = "You";
-    let isLeft = false;
-    addMessage(sender, text, isLeft);
+    let pastMessages = gptMessages.concat([{role: "user", content: text}]);
+    get_gpt_response(pastMessages).then((completion) => {
+      let response = completion.choices[0].message.content;
+      setGptMessages([...gptMessages, {role: "user", content: text}, {role: "assistant", content: response}]);
+      addMessages([{sender: "You", text: text, isLeft: false}, {sender: "Assistant", text: response, isLeft: true}]);
+    })
   }
 
   return (

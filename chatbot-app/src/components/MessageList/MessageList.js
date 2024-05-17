a7e93cbb0616838174adfa55eab7c4bbce8fc611
Devon Friend <devon.fri.2000@gmail.com>
2024-05-16 19:44:40 -0700

    Add message list

---
+++ chatbot-app/src/components/MessageList/MessageList.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import MessageBox from '../MessageBox/MessageBox';
+import './MessageList.css';
+
+const MessageList = ({initialMessages}) => {
+  const [messages, setMessages] = useState([]);
+
+  const addMessage = (sender, text, isLeft) => {
+    setMessages([...messages, {sender, text, isLeft }]);
+  };
+
+  return (
+    <div className="message-list">
+      {messages.map((msg, index) => (
+        <MessageBox key={index} sender={msg.sender} message={msg.text} isLeft={msg.isLeft} />
+      ))}
+      <button onClick={() => addMessage('Assistant', 'New message!', true)}>Add Left Message</button>
+      <button onClick={() => addMessage('You', 'New message!', false)}>Add Right Message</button>
+    </div>
+  );
+};
+
+MessageList.propTypes = {
+    initialMessages: PropTypes.arrayOf(PropTypes.object)
+}
+
+MessageList.defaultProps = {
+    initialMessages: []
+}
+
+export default MessageList;
